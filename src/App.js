ce9f35f2adb65a143f64dcc80ad2bcf51d172e34
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-10-02 10:02:24 +0300

    Create a ul lis nested in li and onclick event for Button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const[todos,setTodos]=useState(
+    [
+      {id: 1,name:"Wake up"},
+      {id :2,name:"prepare braekfast"},
+      {id :3,name:"Go to church"},
+    ] );
+    function addTodo(){
+      setTodos ([...todos,[{id:4,name:"New-Item"}]])
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+     
+      <ul>
+        {
+          todos.map( todo => <li key={todo.id}>{todo.name}</li>)
+        }
+      </ul>
+      <button onClick={addTodo}>addTodo</button>
+      
+       </div>
+  )
 }
 
 export default App;
